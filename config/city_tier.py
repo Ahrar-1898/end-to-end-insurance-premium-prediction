a71551b679dd40392c5cce179e2cc1180fc90ba8
Ahrar-1898 <ahrarsiddiqui74@gmail.com>
2026-04-04 23:32:53 +0530

    Initial commit: Insurance Premium Prediction ML project

---
+++ config/city_tier.py
@@ -0,0 +1,13 @@
+tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Hyderabad", "Chennai", "Kolkata"]
+tier_2_cities = [
+    "Pune", "Ahmedabad", "Jaipur", "Lucknow", "Chandigarh",
+    "Surat", "Indore", "Bhopal", "Nagpur", "Coimbatore",
+    "Kochi", "Visakhapatnam", "Patna", "Vadodara", "Ludhiana",
+    "Agra", "Nashik", "Faridabad", "Meerut", "Rajkot",
+    "Varanasi", "Amritsar", "Prayagraj", "Jabalpur", "Gwalior",
+    "Vijayawada", "Madurai", "Raipur", "Ranchi", "Dehradun",
+    "Jodhpur", "Udaipur", "Shimla", "Mysore", "Hubli",
+    "Mangalore", "Tiruchirappalli", "Salem", "Bareilly", "Aligarh",
+    "Moradabad", "Gorakhpur", "Noida", "Ghaziabad", "Aurangabad",
+    "Durgapur", "Asansol", "Siliguri", "Kolhapur", "Warangal"
+]
